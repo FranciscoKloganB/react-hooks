0718391e27b7ba0e3ea9b71d232bc62e82fd648f
Francisco Barros <francisco.teixeira.de.barros@outlook.com>
2021-09-29 20:05:39 +0100

    Finish extra miles 1, 2 and 3.

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -3,18 +3,43 @@
 
 import * as React from 'react'
 
+function useLocalStorageState(key, defaultValue = '') {
+  const [value, setValue] = React.useState(
+    () => window.localStorage.getItem(key) || defaultValue,
+  )
+
+  React.useEffect(() => {
+    window.localStorage.setItem(key, value)
+  }, [key, value])
+
+  return [value, setValue]
+}
+
+// 4. 💯 flexible localStorage hook - use json stringify and parse to accept any 'value' in our custom hook.
+
 function Greeting({initialName = ''}) {
   // 🐨 initialize the state to the value from localStorage
   // 💰 window.localStorage.getItem('name') || initialName
-  const [name, setName] = React.useState(
-    window.localStorage.getItem('name') || initialName,
-  )
+  // const [name, setName] = React.useState(
+  //   window.localStorage.getItem('name') || initialName,
+  // )
+  // 1. 💯 Use lazy state initialization to avoid reading from local storage after the component is first rendered
+  // const [name, setName] = React.useState(() => window.localStorage.getItem('name') || initialName)
 
   // 🐨 Here's where you'll use `React.useEffect`. The callback should set the `name` in localStorage.
   // 💰 window.localStorage.setItem('name', name)
-  React.useEffect(() => {
-    window.localStorage.setItem('name', name)
-  })
+  // React.useEffect(() => {
+  //   window.localStorage.setItem('name', name)
+  // })
+  // 2. 💯  Use the useEffect dependencies argument to ensure that useEffect only stores `name` to local storage
+  // when the value of `name` is updated and not when a parent re-renders.
+  // React.useEffect(() => {
+  //   window.localStorage.setItem('name', name)
+  // }, [name])
+
+  // 3. 💯 custom hook - Remaining DRY and clean is important. Define a custom hook, that is invoked
+  // by useEffect within this component, but could be used in another components requiring similar behaviours.
+  const [name, setName] = useLocalStorageState('name', initialName)
 
   function handleChange(event) {
     setName(event.target.value)
@@ -36,7 +61,7 @@ function Greeting({initialName = ''}) {
 }
 
 function App() {
-  return <Greeting initialName="Francisco Barros"/>
+  return <Greeting initialName="Francisco Barros" />
 }
 
 export default App
